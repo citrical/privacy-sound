e74e59e9533c3fb1375315b7e7f4db3639be8905
Francisco Limón <franciscolimonleon@gmail.com>
2026-05-17 11:55:28 +0200

    feat(users): add role helpers to model

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
@@ -28,5 +28,25 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    /**
+     * Check if the user has an admin role.
+     *
+     * @return bool
+     */
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    /**
+     * Check if the user has an standard role.
+     *
+     * @return bool
+     */
+    public function isStandard(): bool
+    {
+        return $this->role === 'user';
     }
 }
