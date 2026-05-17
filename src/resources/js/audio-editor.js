e40b7e64d364869ded3ececee1db5a69053c0a16
Francisco Limón <franciscolimonleon@gmail.com>
2026-05-17 18:39:45 +0200

    feat(audio): add WaveSurfer.js and audio-editor entry point

---
+++ src/resources/js/audio-editor.js
@@ -0,0 +1,7 @@
+import WaveSurfer from 'wavesurfer.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions.esm.js'
+import TimelinePlugin from 'wavesurfer.js/dist/plugins/timeline.esm.js'
+
+window.WaveSurfer = WaveSurfer
+window.RegionsPlugin = RegionsPlugin
+window.TimelinePlugin = TimelinePlugin
