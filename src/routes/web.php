4dbb43237d09a52962b079a3f4d7938d807df9c4
Francisco Limón <franciscolimonleon@gmail.com>
2026-05-23 13:31:16 +0200

    feat(admin): add admin routes with middleware protection

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AudioController;
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -25,6 +26,13 @@ Route::middleware('auth')->group(function () {
     Route::post('/audios/{audio}/process', [AudioController::class, 'process'])->name('audios.process');
     Route::get('/audios/{audio}/stream-processed', [AudioController::class, 'streamProcessed'])->name('audios.stream-processed');
     Route::get('/audios/{audio}/download', [AudioController::class, 'download'])->name('audios.download');
+});
+
+Route::middleware(['auth', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('/users', [AdminController::class, 'users'])->name('users');
+    Route::delete('/users/{user}', [AdminController::class, 'destroyUser'])->name('users.destroy');
+    Route::delete('/orphans/{filename}', [AdminController::class, 'destroyOrphan'])->name('orphans.destroy');
 });
 
 require __DIR__.'/auth.php';
