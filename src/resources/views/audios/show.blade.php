78861e5f4dcc307bd064155245cd42ecfe4f295d
Francisco Limón <franciscolimonleon@gmail.com>
2026-05-23 13:08:17 +0200

    feat(audio): add stream-processed route and update waveform player

--- src/resources/views/audios/show.blade.php
+++ src/resources/views/audios/show.blade.php
@@ -47,7 +47,7 @@
             {{-- Formulario de procesamiento --}}
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6">
-                    <form id="process-form" action="#" method="POST">
+                    <form id="process-form" action="{{ route('audios.process', $audio) }}" method="POST">
                         @csrf
                         <input type="hidden" name="regions" id="regions-input" value="[]">
                         <x-primary-button id="btn-process">
@@ -73,7 +73,7 @@
         container: '#waveform',
         waveColor: '#6b7280',
         progressColor: '#F24B4B',
-        url: '{{ route('audios.stream', $audio) }}',
+        url: '{{ $audio->isProcessed() ? route('audios.stream-processed', $audio) : route('audios.stream', $audio) }}',
         plugins: [regions, timeline],
     })
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -22,6 +22,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/audios/{audio}', [AudioController::class, 'destroy'])->name('audios.destroy');
     Route::get('/audios/{audio}/stream', [AudioController::class, 'stream'])->name('audios.stream');
     Route::get('/audios/{audio}', [AudioController::class, 'show'])->name('audios.show');
+    Route::post('/audios/{audio}/process', [AudioController::class, 'process'])->name('audios.process');
+    Route::get('/audios/{audio}/stream-processed', [AudioController::class, 'streamProcessed'])->name('audios.stream-processed');
 });
 
 require __DIR__.'/auth.php';
