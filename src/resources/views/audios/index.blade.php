2554c623f4812e4b661ad9f201938009462bdd32
Francisco Limón <franciscolimonleon@gmail.com>
2026-05-29 04:30:41 +0200

    feat(ui): remove flash message blocks from views

--- src/resources/views/audios/index.blade.php
+++ src/resources/views/audios/index.blade.php
@@ -8,13 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-            {{-- Mensajes de éxito --}}
-            @if (session('success'))
-                <div class="mb-4 p-4 bg-green-100 text-green-700 rounded-lg">
-                    {{ session('success') }}
-                </div>
-            @endif
-
             {{-- Formulario de subida --}}
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mb-6">
                 <div class="p-6">
